ac8a4c8747ac2111943c3a29c8ed04e80db35ab4
dobalissimo <dobalissimo@gmail.com>
2023-06-08 17:23:46 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+let boyorgirl = prompt('¿Eres un chico, una chica o prefieres no utilizar un pronombre?').toLowerCase();
+let nombre = prompt('¿Cuál es tu nombre?');
+let comida = prompt('¿Cuál es tu comida favorita?');
+let promptpokemon = 0; 
+while (promptpokemon < 1 || promptpokemon > 9) {
+   userInput = prompt('Elige tu pokemon utilizando su número, del 1 al 9');
+ 
+   if (isNaN(userInput)) {
+     alert('¡Ingresa un valor numérico!');
+   } else {
+     promptpokemon = parseInt(userInput);
+   }
+ 
+   if (promptpokemon < 1 || promptpokemon > 9) {
+     alert('¡Del 1 al 9 te dije!');
+   }
+ }
+let nropokemon = parseInt(promptpokemon);
+let pokemonName;
+switch (nropokemon) {
+  case 1:
+    pokemonName = 'Bulbasaur';
+    break;
+  case 2:
+    pokemonName = 'Ivysaur';
+    break;
+  case 3:
+    pokemonName = 'Venusaur';
+    break;
+  case 4:
+    pokemonName = 'Charmander';
+    break;
+  case 5:
+    pokemonName = 'Charmeleon';
+    break;
+  case 6:
+    pokemonName = 'Charizard';
+    break;
+  case 7:
+    pokemonName = 'Squirtle';
+    break;
+  case 8:
+    pokemonName = 'Wartortle';
+    break;  
+  case 9:
+     pokemonName = 'Blastoise';
+     break;
+  default:
+    pokemonName = 'MissingNo';
+}
+
+function saludarEntrenador() {
+   if (boyorgirl === "chico") {
+     alert('¡Hola, entrenador ' + nombre + ' de pueblo ' + comida + '!' + ' Has elegido a ' + pokemonName + '.');
+   } else if (boyorgirl === "chica") {
+     alert('¡Hola, entrenadora ' + nombre + ' de pueblo ' + comida + '!' + ' Has elegido a ' + pokemonName + '.');
+   } else {
+     alert('Te damos la bienvenida, ' + nombre + ' de pueblo ' + comida + '!' + ' Has elegido a ' + pokemonName + '.');
+   }
+ }
+
+ saludarEntrenador();
+
+ 
