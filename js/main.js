ad4d349b59a73d355b8c783b12d2aad3ee2f3972
dobalissimo <dobalissimo@gmail.com>
2023-06-08 19:50:49 -0300

    agregando los comentarios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+//preguntas para la función final
+
 let boyorgirl = prompt('¿Eres un chico, una chica o prefieres no utilizar un pronombre?').toLowerCase();
 let nombre = prompt('¿Cuál es tu nombre?');
 let comida = prompt('¿Cuál es tu comida favorita?');
+
+//número del pokemon del 1 al 9, dejando alertas también para los casos donde no completan con los valores requeridos
+
+
 let promptpokemon = 0; 
 while (promptpokemon < 1 || promptpokemon > 9) {
    userInput = prompt('Elige tu pokemon utilizando su número, del 1 al 9');
@@ -16,6 +22,9 @@ while (promptpokemon < 1 || promptpokemon > 9) {
    }
  }
 let nropokemon = parseInt(promptpokemon);
+
+//acá se le asigna el nombre al pokemon usando el número que ingresaron
+
 let pokemonName;
 switch (nropokemon) {
   case 1:
@@ -48,6 +57,9 @@ switch (nropokemon) {
   default:
     pokemonName = 'MissingNo';
 }
+
+//función donde se conjugan todos los datos para saludar al usuario y se le informa de su pokemon elegido
+
 
 function saludarEntrenador() {
    if (boyorgirl === "chico") {
